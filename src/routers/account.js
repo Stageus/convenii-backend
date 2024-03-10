b1036d907f9a1a8e1539eb962551b2c2283fe38d
gongsiri <kjykjy3314@naver.com>
2024-03-10 14:51:23 +0000

    update: checkCondition.js modules-> middleware

--- src/routers/account.js
+++ src/routers/account.js
@@ -7,7 +7,7 @@ const patternConfig = require("../config/patternConfig");
 const emailPattern = patternConfig.emailPattern;
 const pwPattern = patternConfig.pwPattern;
 const nicknamePattern = patternConfig.nicknamePattern;
-const checkCondition = require("../modules/checkCondition");
+const checkCondition = require("../middleware/checkCondition");
 const queryModule = require("../modules/queryModule");
 
 router.post("/", checkCondition("email", emailPattern), checkCondition("pw", pwPattern), checkCondition("nickname", nicknamePattern), checkSame("pw", "pwSame"), async (req, res, next) => {
