16c5144badaee5ef0283ccc4bf75144d2711a0d8
juneh2633 <juneh2633@gmail.com>
2024-03-11 03:44:00 +0900

    feat: product Apis struct
    product api 뼈대 생성

--- src/routers/product.js
+++ src/routers/product.js
@@ -1,4 +1,43 @@
 const router = require("express").Router();
-const pgPool = require("../modules/pgPool");
 
+//productIdx 가져오기
+router.get("/:productIdx", async (req, res, next) => {
+    try {
+    } catch (err) {}
+});
+
+//모든 상품 가져오기
+router.get("/all", async (req, res, next) => {
+    try {
+    } catch (err) {}
+});
+//회사 행사페이지 상품 가져오기
+router.get("/company/:companyIdx", async (req, res, next) => {
+    try {
+    } catch (err) {}
+});
+
+//상품 검색하기
+router.get("/company/search", async (req, res, next) => {
+    try {
+    } catch (err) {}
+});
+
+//상품 추가하기
+router.post("/", async (req, res, next) => {
+    try {
+    } catch (err) {}
+});
+
+// productIdx 수정하기
+router.put("/:productIdx", async (req, res, next) => {
+    try {
+    } catch (err) {}
+});
+
+//productIdx 삭제하기
+router.delete("/:productIdx", async (req, res, next) => {
+    try {
+    } catch (err) {}
+});
 module.exports = router;
