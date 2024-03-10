55d6d60df4b2e6a9a1e99780572acb5f56a006f6
gongsiri <kjykjy3314@naver.com>
2024-03-10 16:13:25 +0000

    에러

--- src/routers/account.js
+++ src/routers/account.js
@@ -39,6 +39,43 @@ router.post("/", checkCondition("email", emailPattern), checkCondition("pw", pwP
 //로그인
 router.post("/login", checkCondition("email", emailPattern), checkCondition("pw", pwPattern), async (req, res, next) => {
     const { email, pw } = req.body;
+    try {
+        const trimEmail = email.trim();
+        const sql = "SELECT * FROM account WHERE email =$1 AND pw=$2";
+        const queryData = await queryModule(sql, [trimEmail, pw]);
+
+        if (queryData.length == 0) {
+            const error = new Error("로그인 실패");
+            error.status = 401;
+            throw error;
+        }
+
+        const idx = queryData[0].idx;
+        const uniqueIdx = uuid.v4();
+
+        const token = jwt.sign(
+            {
+                "idx": idx,
+                "email": queryData[0].email,
+                "rank": queryData[0].rank_idx,
+                "uuid": uniqueIdx
+            },
+            process.env.SECRET_KEY,
+            {
+                "issuer": queryData[0].nickname,
+                "expiresIn": "10m" // 임시로 10분
+            }
+        );
+
+        res.cookie("accessToken", token, {
+            httpOnly: true,
+            secure: false
+        });
+
+        res.sendStatus(201);
+    } catch (error) {
+        next(error)
+    }
 })
 
 module.exports = router
