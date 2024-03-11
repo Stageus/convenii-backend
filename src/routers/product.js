9086e73e1338c1f4a87ef15bafd57ca9a99ad9d3
juneh2633 <juneh2633@gmail.com>
2024-03-11 19:21:52 +0900

    feat: product sql
    product와 history를 조회하는 sql문 작성

--- src/routers/product.js
+++ src/routers/product.js
@@ -3,10 +3,76 @@ const router = require("express").Router();
 //productIdx 가져오기
 router.get("/:productIdx", async (req, res, next) => {
     const { productIdx } = req.params;
+    const { company_infocountIdx } = a;
     const result = {
-        data: {},
+        data: null,
     };
+
     try {
+        const productSql = `SELECT 
+                                idx AS product_idx,
+                                name,
+                                price,
+                                image_url,
+                                score,
+                                created_at,
+                                CASE
+                                    WHEN (SELECT COUNT(*) FROM bookmark WHERE product_idx  = $1 AND company_infocount_idx = $2 ) >= 1
+                                    THEN 1
+                                    ELSE 0
+                                END AS bookmarked
+                            FROM
+                                product 
+                            WHERE idx = $1 AND deleted_at is NULL`;
+
+        const eventSql = `
+        WITH month_array AS (
+            SELECT to_char(date_trunc('month', current_date) - interval '1 month' * series, 'YYYY-MM') AS month
+            FROM generate_series(0, 9) AS series
+        ),
+        company_array AS (
+            SELECT idx, name 
+            FROM company
+        ),
+        event_array AS (
+            SELECT 
+                to_char(eh.start_date, 'YYYY-MM') AS month,
+                company.name AS company_name,
+                event.type AS event_type
+            FROM 
+                event_history 
+            JOIN 
+                event ON event_history.event_idx = event.idx
+            JOIN 
+                company ON event_history.company_idx = company.idx
+            WHERE 
+                event_history.product_idx = $1 AND
+                event_history.start_date >= (SELECT min(date_trunc('month', current_date) - interval '1 month' * series) FROM generate_series(0, 9) AS series)
+        ),
+        result AS (
+            SELECT 
+                month_array.month,
+                company_array.name AS company_name,
+                COALESCE(event_array.event_type, 'null') AS event_type
+            FROM 
+                month_array 
+            CROSS JOIN 
+                company_array 
+            LEFT JOIN 
+                event_array  ON month_array.month = event_array.month AND company_array.name = event_array.company_name
+        )
+        SELECT 
+            month, 
+            json_object_agg(company_name, event_type) AS events
+        FROM 
+            result
+        GROUP BY 
+            month
+        ORDER BY 
+            month DESC   
+        `;
+        const productQueryResult = await pgPool.query(productSql, [productIdx, company_infocountIdx]);
+        const eventQueryResult = await pgPool.query(eventSql, [productIdx]);
     } catch (err) {}
 });
 
