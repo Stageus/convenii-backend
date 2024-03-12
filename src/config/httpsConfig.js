57336657eeeecbb8d6a7818847a6390f09a28e7d
juneh2633 <juneh2633@gmail.com>
2024-03-12 18:54:31 +0900

    feat: modify httpConfig to function

--- src/config/httpsConfig.js
+++ src/config/httpsConfig.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = {
-    key: fs.readFileSync(path.join(__dirname, "../keys/key.pem")),
-    cert: fs.readFileSync(path.join(__dirname, "../keys/cert.pem")),
-    //ca:
-    passphrase: process.env.HTTPS_PASSPHRASE,
+module.exports = () => {
+    return {
+        key: fs.readFileSync(path.join(__dirname, "../keys/key.pem")),
+        cert: fs.readFileSync(path.join(__dirname, "../keys/cert.pem")),
+        //ca:
+        passphrase: process.env.HTTPS_PASSPHRASE,
+    };
 };
