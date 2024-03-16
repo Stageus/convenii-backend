a877963f6d2bb885f963b3710fc46e5876fb8a8c
공실 <kjykjy3314@naver.com>
2024-03-17 01:22:09 +0900

    fix:transporter.js
    transporter 파일 수정

--- src/modules/transporter.js
+++ src/modules/transporter.js
@@ -1,13 +1,17 @@
 const nodemailer = require("nodemailer");
+const mailerConfig = require("../config/mailerConfig");
 
-const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: process.env.EMAIL_SECURE,
-    auth: {
-        user: process.env.EMAIL_USER,
-        pass: process.env.EMAIL_PASSWORD
-    } // config로
-});
+async function sendVerificationEmail(email, verificationCode) {
+    const transporter = nodemailer.createTransport(mailerConfig);
 
-module.exports = transporter;
+    const mailOptions = {
+        from: process.env.EMAIL_USER,
+        to: email,
+        subject: "이메일 인증",
+        text: `인증번호 : ${verificationCode}`,
+    };
+
+    await transporter.sendMail(mailOptions);
+}
+
+module.exports = sendVerificationEmail;
