8e2041278a91f515a14cbb40d3b670fc49be2660
공실 <kjykjy3314@naver.com>
2024-03-17 16:49:21 +0900

    create:
    API 뼈대 생성

--- src/routers/bookmark.js
+++ src/routers/bookmark.js
@@ -1,3 +1,30 @@
 const router = require("express").Router();
 
+// 북마크 등록하기
+router.post("/", async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 북마크 가져오기
+router.get("/all", async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+// 북마크 삭제하기
+router.delete("/", async (req, res, next) => {
+    try {
+
+    } catch (error) {
+        next(error);
+    }
+});
+
 module.exports = router;
