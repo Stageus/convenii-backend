90f6f37ecd08b97ad11bec5b535f6266166ffeab
공실 <kjykjy3314@naver.com>
2024-03-21 23:23:21 +0900

    feat: GET/bookmark/all
    북마크 가져오기 api 코드 생성

--- src/routers/bookmark.js
+++ src/routers/bookmark.js
@@ -5,27 +5,26 @@ const pgPool = require("../modules/pgPool");
 // 북마크 등록하기
 router.post("/product/:productIdx", loginAuth, async (req, res, next) => {
     const { productIdx } = req.params;
-    const idx = req.user.idx;
+    const accountIdx = req.user.idx;
     try {
-        const productSql = 'SELECT * FROM product WHERE idx = $1';
-        const productQueryData = await pool.query(productSql, [productIdx]);
+        const prodcutExistingSql = "SELECT * FROM product WHERE idx = $1";
+        const productQueryData = await pgPool.query(prodcutExistingSql, [productIdx]);
         if (productQueryData.rows.length === 0) {
             const error = new Error("존재하지 않는 상품임");
             error.status = 400;
             throw error;
         }
 
-        const insertSql = `
-            INSERT INTO bookmark (account_idx, product_idx) 
-            VALUES ($1,$2)
-            ON CONFLICT (product_idx) DO NOTHING`;
-        const insertQueryData = await pgPool.query(insertSql, [idx, productIdx]);
+        const bookmarkExistingSql = "SELECT * FROM bookmark WHERE account_idx=$1 AND product_idx=$2";
+        const bookmarkQueryData = await pgPool.query(bookmarkExistingSql, [accountIdx, productIdx]);
 
-        if (insertQueryData.rows.length == 0) {
+        if (bookmarkQueryData.rows.length > 0) {
             const error = new Error("이미 북마크 되어 있는 상품임");
             error.status = 401;
             throw error;
         }
+        const insertSql = "INSERT INTO bookmark (account_idx, product_idx) VALUES ($1,$2)";
+        await pgPool.query(insertSql, [accountIdx, productIdx]);
 
         res.status(201).send();
     } catch (error) {
@@ -34,11 +33,33 @@ router.post("/product/:productIdx", loginAuth, async (req, res, next) => {
 });
 
 // 북마크 가져오기
-router.get("/all", async (req, res, next) => {
+router.get("/all", loginAuth, async (req, res, next) => {
+    const accountIdx = req.user.idx;
+    const result = {
+        data: null,
+    };
     try {
-        const result = {
-            data: null,
-        };
+        const sql = `
+            SELECT
+                p.idx,
+                p.category_idx,
+                p.name,
+                p.price,
+                p.image_url,
+                p.score
+            FROM 
+                product p
+            INNER JOIN
+                bookmark b ON p.idx = b.product_idx
+            WHERE 
+                b.account_idx = $1
+                AND
+                p.deleted_at IS NULL
+        `;
+        const queryData = await pgPool.query(sql, [accountIdx]);
+        result.data = queryData.rows;
+
+        res.status(200).send(result);
     } catch (error) {
         next(error);
     }
