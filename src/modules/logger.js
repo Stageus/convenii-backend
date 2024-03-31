1fb90744c7c41a205cb1ac5c4fde4f90668b2c79
juneh2633 <juneh2633@gmail.com>
2024-03-31 15:11:44 +0900

    feat:logger

--- src/modules/logger.js
+++ src/modules/logger.js
@@ -1,9 +1,8 @@
 const winston = require("winston");
 const winstonDaily = require("winston-daily-rotate-file");
 const { combine, timestamp, label, printf } = winston.format;
-
-const logDir = `../../logs`;
-
+const path = require("path");
+const logDir = path.join(__dirname, "../../logs");
 const logFormat = printf(({ level, message, label, timestamp }) => {
     return `${timestamp} [${label}] ${level}: ${message}`;
 });
