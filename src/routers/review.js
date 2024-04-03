2ec20eb5f831796531760a084b03065ea871b831
juneh2633 <juneh2633@gmail.com>
2024-04-03 19:56:59 +0900

    refactor: review.js

--- src/routers/review.js
+++ src/routers/review.js
@@ -1,29 +1,37 @@
 const router = require("express").Router();
 const checkCondition = require("../middlewares/checkCondition");
+const checkAuthStatus = require("../middlewares/checkAuthStatus");
 const loginAuth = require("../middlewares/loginAuth");
 const pgPool = require("../modules/pgPool");
-
+const wrapper = require("../modules/wrapper");
+const query = require("../modules/query");
+const { Exception, NotFoundException, BadRequestException, UnauthorizedException, ForbiddenException } = require("../modules/Exception");
 /////////////-------review---------////////////////////
 //  POST/product/:productIdx        => 리뷰 추가하기
 //  GET/product/:productIdx         =>  모든 리뷰 가져오기
 /////////////////////////////////////////////////////
 
 //productIdx의 리뷰 추가하기
-router.post("/product/:productIdx", checkCondition("score"), checkCondition("content"), loginAuth, async (req, res, next) => {
-    const { score, content } = req.body;
-    const { productIdx } = req.params;
-    const accountIdx = req.user.idx;
+router.post(
+    "/product/:productIdx",
+    checkCondition("score"),
+    checkCondition("content"),
+    loginAuth,
+    wrapper(async (req, res, next) => {
+        const { score, content } = req.body;
+        const { productIdx } = req.params;
+        const accountIdx = req.user.idx;
 
-    const client = await pgPool.connect();
-    try {
-        await client.query("BEGIN");
+        const client = await pgPool.connect();
+        try {
+            await client.query("BEGIN");
 
-        const reviewSql = `
+            const reviewSql = `
             INSERT INTO
                 review (product_idx, account_idx, content, score)
             VALUES
                 ($1,$2,$3,$4)`;
-        const scoreSql = `
+            const scoreSql = `
             UPDATE product SET score = (
                 SELECT
                     AVG (score)
@@ -34,36 +42,40 @@ router.post("/product/:productIdx", checkCondition("score"), checkCondition("con
             )
             WHERE idx = $1
         `;
-        const reviewQueryResult = await client.query(reviewSql, [productIdx, accountIdx, content, score]);
-        const scoreQueryResult = await client.query(scoreSql, [productIdx]);
-        if (reviewQueryResult.rowCount === 0) {
-            const error = new Error("insert review fail");
-            error.status = 400;
-            throw error;
+            const reviewQueryResult = await client.query(reviewSql, [productIdx, accountIdx, content, score]);
+            const scoreQueryResult = await client.query(scoreSql, [productIdx]);
+            if (reviewQueryResult.rowCount === 0) {
+                throw new BadRequestException("insert review fail");
+            }
+            if (scoreQueryResult.rowCount === 0) {
+                throw new UnauthorizedException("update score fail");
+            }
+            await client.query("COMMIT");
+            res.status(201).send();
+        } catch (err) {
+            await client.query("ROLLBACK");
+            next(err);
+        } finally {
+            await client.release();
         }
-        if (scoreQueryResult.rowCount === 0) {
-            const error = new Error("update score fail");
-            error.status = 401;
-            throw error;
-        }
-        await client.query("COMMIT");
-        res.status(201).send();
-    } catch (err) {
-        await client.query("ROLLBACK");
-        next(err);
-    }
-});
+    })
+);
 
 //productIdx의 모든 리뷰 가져오기
-router.get("/product/:productIdx", async (req, res, next) => {
-    const { productIdx } = req.params;
-    const { page } = req.query;
-    const pageSizeOption = 10;
-    const result = {
-        data: null,
-    };
-    try {
-        const sql = `
+router.get(
+    "/product/:productIdx",
+    checkAuthStatus,
+    wrapper(async (req, res, next) => {
+        const { productIdx } = req.params;
+        const { page } = req.query;
+        const pageSizeOption = 10;
+
+        if (!page || isNaN(parseInt(page, 10)) || page <= 0) {
+            throw new BadRequestException("page 입력 오류");
+        }
+
+        const reviews = await query(
+            `
             SELECT
                 r.idx, r.product_idx, a.nickname, r.content, r.score, r.created_at
             FROM
@@ -74,14 +86,15 @@ router.get("/product/:productIdx", async (req, res, next) => {
                 r.product_idx = $1
             ORDER BY r.idx DESC
             LIMIT $2 OFFSET $3
-        `;
-        const queryResult = await pgPool.query(sql, [productIdx, pageSizeOption, (parseInt(page) - 1) * pageSizeOption]);
+            `,
+            [productIdx, pageSizeOption, (parseInt(page) - 1) * pageSizeOption]
+        );
 
-        result.data = queryResult.rows;
-        res.status(200).send(result);
-    } catch (err) {
-        next(err);
-    }
-});
+        res.status(200).send({
+            data: reviews.rows,
+            authStatus: req.isLogin,
+        });
+    })
+);
 
 module.exports = router;
