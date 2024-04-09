6bb64304899748999db6f88dcbaa5034df76e1f0
juneh2633 <juneh2633@gmail.com>
2024-04-09 23:13:18 +0900

    feat: model/Category.js

---
+++ src/model/Category.js
@@ -0,0 +1,21 @@
+class Category {
+    /**
+     * @type {number}
+     */
+    idx;
+    /**
+     * @type {string}
+     */
+    name;
+    /**
+     *
+     * @param {{
+     *  idx: number;
+     *  name: string;
+     * }} data
+     */
+    constructor(data) {
+        this.idx = data.idx;
+        this.name = data.name;
+    }
+}
