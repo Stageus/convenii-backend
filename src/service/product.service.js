2dc1e38a8514792f1b3fbef23448b035f8e36e58
juneh2633 <juneh2633@gmail.com>
2024-04-10 17:18:55 +0900

    chore: change eventHistoryData output eventType to eventIdx

--- src/service/product.service.js
+++ src/service/product.service.js
@@ -1,14 +1,19 @@
 const CreateProductDto = require("../dto/CreateProductDto");
 
-const { getProductData } = require("../repository/productRepository");
+const { getProductData, getEventHistoryData } = require("../repository/productRepository");
 
 const getProductByIdx = async (user, productIdx) => {
     const productData = await getProductData(user.idx, productIdx);
     if (!productData) {
         throw new NotFoundException("Cannot find product");
     }
+    const productDto = new CreateProductDto(productData);
+    const product = productDto.createProduct();
 
-    const product = CreateProductDto.createProduct(productData);
+    const eventHistoryData = await getEventHistoryData(productIdx);
+    if (!eventHistoryData) {
+        throw new NotFoundException("Cannot find product's eventHistory ");
+    }
 
     return {
         product,
