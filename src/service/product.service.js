4c6d88a9ddd1eb4cf7bbc036f469a3a324d6b38d
juneh2633 <juneh2633@gmail.com>
2024-04-10 21:43:01 +0900

    feat: getProductAll

--- src/service/product.service.js
+++ src/service/product.service.js
@@ -1,6 +1,6 @@
 const CreateProductDto = require("../dto/CreateProductDto");
 const CreateEventHistoryDto = require("../dto/CreateEventHistoryDto");
-const { getProductData, getEventHistoryData } = require("../repository/productRepository");
+const { getProductData, getEventHistoryData, getProductsData } = require("../repository/productRepository");
 const { NotFoundException } = require("../modules/Exception");
 const EventHistory = require("../entity/EventHistory");
 const Product = require("../entity/Product");
@@ -15,6 +15,7 @@ const Product = require("../entity/Product");
  * >}
  */
 const getProductByIdx = async (user, productIdx) => {
+    //productData
     const productData = await getProductData(user.idx, productIdx);
     if (!productData) {
         throw new NotFoundException("Cannot find product");
@@ -23,10 +24,13 @@ const getProductByIdx = async (user, productIdx) => {
     const productDto = new CreateProductDto(productData);
     const product = productDto.createProduct();
 
+    // eventHistoryData
     const eventHistoryData = await getEventHistoryData(productIdx);
     if (!eventHistoryData) {
         throw new NotFoundException("Cannot find product's eventHistory ");
     }
+
+    //data transfer
     const eventHistory = [];
     eventHistoryData.forEach((data) => {
         const eventHistoryDto = new CreateEventHistoryDto(data);
@@ -39,4 +43,57 @@ const getProductByIdx = async (user, productIdx) => {
     };
 };
 
-module.exports = { getProductByIdx };
+/**
+ *
+ * @param {req.user} user
+ * @param {number} page
+ * @returns {Promise<Array<{
+ *          product:Product
+ *          events:EventHistory
+ *      }
+ * >}
+ */
+const getProductAll = async (user, page) => {
+    const pageSizeOption = 10;
+    if (!page || isNaN(parseInt(page, 10)) || page <= 0) {
+        throw new BadRequestException("page 입력 오류");
+    }
+
+    const productsData = await getProductsData(user.idx, page, pageSizeOption);
+    if (!productsData) {
+        throw new BadRequestException("Cannot find products");
+    }
+
+    const data = [];
+
+    productsData.forEach((productDataSet) => {
+        const productData = {
+            idx: productDataSet.idx,
+            categoryIdx: productDataSet.categoryIdx,
+            name: productDataSet.name,
+            price: productDataSet.price,
+            productImg: productDataSet.productImg,
+            score: productDataSet.score,
+            createdAt: productDataSet.createdAt,
+            bookmarked: productDataSet.bookmarked,
+        };
+
+        const eventHistoryData = {
+            month: productDataSet.month,
+            events: productDataSet.events,
+        };
+        const productDto = new CreateProductDto(productData);
+        const product = productDto.createProduct();
+
+        const eventHistoryDto = new CreateEventHistoryDto(eventHistoryData);
+        const eventHistory = eventHistoryDto.createEventHistory();
+
+        data.push({
+            product,
+            eventHistory,
+        });
+    });
+
+    return data;
+};
+module.exports = { getProductByIdx, getProductAll };
