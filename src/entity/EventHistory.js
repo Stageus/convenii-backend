0a973b32c0e93887286b5932be851d96e48081f0
juneh2633 <juneh2633@gmail.com>
2024-04-13 04:32:43 +0900

    fix: change data Event, EventHistory entity correctly

--- src/entity/EventHistory.js
+++ src/entity/EventHistory.js
@@ -1,33 +1,51 @@
 class EventHistory {
     /**
+     * @type {number}
+     */
+    idx;
+
+    /**
+     * @type {number}
+     */
+    company_idx;
+
+    /**
+     * @type {number}
+     */
+    product_idx;
+
+    /**
+     * @type {number}
+     */
+    event_idx;
+
+    /**
      * @type {Date}
      */
-    month;
+    start_date;
 
     /**
-     * @type {
-     *      Array<{
-     *          companyIdx: number,
-     *          eventIdx: number,
-     *          price: string | null
-     *      }>
-     * }
+     * @type {string}
      */
-    events;
+    price;
 
     /**
-     * @param {{
-     *  month: string,
-     *  events: Array<{
-     *      companyIdx: number,
-     *      eventIdx: number,
-     *      price: string | null
-     *  }>
-     * }} data
+     * @param {
+     *  idx: number;
+     *  company_idx: number;
+     *  product_idx: number;
+     *  event_idx: number|null;
+     *  start_date: Date;
+     *  price: string;
+     * } data
      */
     constructor(data) {
-        this.month = data.month;
-        this.events = data.events;
+        this.idx = data.idx;
+        this.company_idx = data.company_idx;
+        this.product_idx = data.product_idx;
+        this.event_idx = data.event_idx;
+        this.start_date = data.start_date;
+        this.price = data.price;
     }
 }
 
