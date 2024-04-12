869a788131cc5ca767ae30bec2ac0ee8bc39e56e
juneh2633 <juneh2633@gmail.com>
2024-04-12 17:23:34 +0900

    feat: patternTest

---
+++ src/modules/patternTest.js
@@ -0,0 +1,22 @@
+const patterns = {
+    email: /^[a-zA-Z0-9]{6,40}@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+    pw: /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{6,20}$/,
+    month: /^\d{4}-(0[1-9]|1[0-2])$/,
+    nickname: /^[a-zA-Z0-9가-힣]{3,10}$/,
+    score: /^[0-5]$/,
+    content: /^$|.{5,100}$/,
+    page: /^[1-9]\d*$/,
+    name: /^[가-힣a-zA-Z0-9]{2,40}$/,
+    price: /^(?:[1-9][0-9]{2,6}|10000000)$/,
+};
+/**
+ *
+ * @param {string} dataName
+ * @param {any} data
+ * @returns {boolean}
+ */
+const test = (dataName, data) => {
+    return patterns[dataName].test(data);
+};
+
+module.exports = pattenrnTest;
