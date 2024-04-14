aa04b18401b8740707fc1c4077f6e47cb48b9db5
juneh2633 <juneh2633@gmail.com>
2024-04-15 07:51:04 +0900

    feat: accountAuth
    기존 adminAuth, checkAuthStatus, loginAuth 를 하나로 통합

---
+++ src/util/middleware/accountAuth.js
@@ -0,0 +1,25 @@
+const { checkLogin } = require("../../account/account.service");
+const { UnauthorizedException } = require("../module/Exception");
+
+/**
+ *
+ * @param {number} needRank
+ * @returns {Function}
+ */
+const accountAuth =
+    (needRank = 0) =>
+    async (req, res, next) => {
+        const token = req.headers.authorization;
+        const user = await checkLogin(token);
+        if (user.rankIdx !== needRank) {
+            let message = "No permission";
+            if (user.authStatus === "expired") {
+                message = "token expired";
+            }
+            next(new UnauthorizedException(message));
+        }
+        req.user = user;
+        next();
+    };
+
+module.exports = accountAuth;
