842296d18fc1922d5cef51b0eb182a6bcd4bf57d
juneh2633 <juneh2633@gmail.com>
2024-04-18 10:11:12 +0900

    feat: review repository

---
+++ src/review/review.repository.js
@@ -0,0 +1,89 @@
+const pgPool = require("../../src/util/module/pgPool");
+const query = require("../util/module/query");
+const InsertReivewDao = require("./dao/insert-review.dao");
+const SelectReviewsDao = require("./dao/select-reviews.dao");
+const UpdateScoreDao = require("./dao/update-score.dao");
+const Review = require("./model/revew.model");
+
+/**
+ *
+ * @param {InsertReivewDao} insertReviewDao
+ * @param {pg.PoolClient} conn
+ * @returns {Promise<pg.QueryResult>}
+ */
+const insertReivew = async (insertReviewDao, conn = pgPool) => {
+    return await query(
+        `
+        INSERT INTO
+            review (product_idx, account_idx, content, score)
+        VALUES
+            ($1,$2,$3,$4)`,
+        [insertReviewDao.productIdx, insertReviewDao.accountIdx, insertReviewDao.content, insertReviewDao.score],
+        conn
+    );
+};
+
+/**
+ *
+ * @param {UpdateScoreDao} updateScoreDao
+ * @param {pg.PoolClient} conn
+ * @returns {Promise<pg.QueryResult>}
+ */
+const updateScore = async (updateScoreDao, conn = pgPool) => {
+    return await query(
+        `
+        UPDATE product SET score = (
+            SELECT
+                AVG (score)
+            FROM
+                review
+            WHERE
+                product_idx = $1
+        )
+        WHERE idx = $1
+        `,
+        [updateScoreDao.productIdx],
+        conn
+    );
+};
+
+/**
+ *
+ * @param {SelectReviewsDao} selectReviewsDao
+ * @param {pg.PoolClient} conn
+ * @returns {Promise<Review[]>}
+ */
+const selectReviews = async (selectReviewsDao, conn = pgPool) => {
+    const queryResult = await query(
+        `
+        SELECT
+            review.idx,
+            review.product_idx,
+            account.nickname,
+            review.content,
+            review.score,
+            review.created_at
+        FROM
+            review
+        JOIN
+            account
+        ON
+            account.idx = review.account_idx
+        WHERE
+            review.product_idx = $1
+        ORDER BY
+            review.idx DESC
+        LIMIT $2 OFFSET $3
+        `,
+        [selectReviewsDao.productIdx, selectReviewsDao.limit, selectReviewsDao.offset],
+        conn
+    );
+
+    return queryResult;
+};
+
+module.exports = {
+    insertReivew,
+    updateScore,
+    selectReviews,
+};
